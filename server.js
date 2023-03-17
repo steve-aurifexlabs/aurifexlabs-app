7da329c9ab0de9bc4474302b1e073db1209c058f
Steve Goldsmith <steve@aurifexlabs.com>
2023-03-17 12:10:10 -0600

    Init node package.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello from the server.')
+var http = require('http')
+
+var server = http.createServer(function(req, res) {
+    res.statusCode(200)
+    res.send('Hello world')
+})
+
+server.listen(8080)
+
+console.log('Server started on port 8080.')
